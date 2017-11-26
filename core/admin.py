3d1cd66edb16d1d276f51caaa994501276afdf0d
Brent Buchholz <brentbuchholz@gmail.com>
2017-11-26 10:09:52 -0600

    trying to fix heroku db

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from core.models import Fragment, Document, Quote, QuoteAccessLogEntry
+from core.models import Fragment, Document, Quote
+#from core.models import QuoteAccessLogEntry
 
 # Register your models here.
 admin.site.register(Fragment)
 admin.site.register(Document)
 #admin.site.register(Quote) #registered below 
-admin.site.register(QuoteAccessLogEntry)
+#admin.site.register(QuoteAccessLogEntry)
 
 # register admin classes for Quote with the decorator
 @admin.register(Quote) 
