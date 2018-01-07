cfbb390472d213496f36185a1d42dd1fe0f2ae77
Brent Buchholz <brentbuchholz@gmail.com>
2018-01-07 10:53:17 -0600

    registered new archivist models with django admin

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,38 @@
 from django.contrib import admin
-from core.models import Fragment, Document, Quote
+from core.models import (
+    Fragment, 
+    Document, 
+    Quote, 
+    MediaTag, 
+    QuoteTagging, 
+    Source, 
+    SourceExcerpt,
+    SourceExcerptTagging,
+    SourceType,
+)
 
 
 # Register your models here.
 admin.site.register(Fragment)
 admin.site.register(Document)
 #admin.site.register(Quote) #registered below 
+admin.site.register(MediaTag)
+admin.site.register(QuoteTagging)
+admin.site.register(Source)
+admin.site.register(SourceExcerpt)
+admin.site.register(SourceExcerptTagging)
+admin.site.register(SourceType)
+
 
 # register admin classes for Quote with the decorator
 @admin.register(Quote) 
 class QuoteAdmin(admin.ModelAdmin):
     list_display = ('text', 'owner', 'public_accessible')
+
+
+
+
+
+
+
+
